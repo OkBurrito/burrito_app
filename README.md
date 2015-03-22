51b83b1eff6a95d6c83bec74d317ac4d4f9309b9
Joe Purdy <joe@poweredbypurdy.com>
2015-03-22 15:45:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+okburrito
+=========
+
+Matching people to their ideal burrito using highly accurate burrito math.
+As seen on imgur: http://imgur.com/gallery/AZ0txE8
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="OkBurrito">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <img class="title" src="img/splash_logo.png">
+            </header>
+        </div>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <article>
+                    <header>
+                        <h1>Greetings Imgurites and other internet denizens</h1>
+                        <p>I apologize for the mess, things have been very busy the last few hours. For now I'm focusing on getting this landing page built so you can stay in touch with any developments on the creation of OkBurrito.</p>
+                    </header>
+                    <section>
+                        <h2>What is OkBurrito?</h2>
+                        <p>OkBurrito was originally envisioned as a place where people could create a profile that matches them to ideal burritos in their area based on highly accurate burrito math. However with the feedback received from the Imgur community I've come to look at many different avenues to take OkBurrito down. At it's core will be the original profile based social networking aspect, however it's my goal to expand the site to be a pioneer in the foodshare realm.</p>
+                    </section>
+                    <section>
+                        <h2>Subscribe for updates</h2>
+                        <p>So I was working on an email subscription thing, but I hit a snag and I won't be able to get it working until I get home from work today (5PM PDT). For now your best place for updates is the Imgur post. If you guys want some other form of updates besides email and Imgur let me know and I'll see about implementing it.</p>
+                    </section>
+                    <footer>
+                        <h3>Want to help?</h3>
+                        <p>If you have an awesome burrito or technology related skill that you think would be a benefit to this project I'd love to hear from you! I've set up an email for people to shoot me ideas and ask questions:
+<!-- Obfuscated to foil the damn spam bots. -->
+<a href="mailto&#58;imgurians&#64;o&#107;&#98;&#117;r&#114;%69&#116;&#37;6F&#46;com?Subject=I%20want%20to%20help%20you%20Party%20God!" target="_blank">&#105;&#109;g&#117;rians&#64;&#111;kburr&#105;to&#46;&#99;om</a></p>
+                    </footer>
+                </article>
+
+                <aside>
+                    <h3>Buy me Booze!</h3>
+                    <p>Because of a binding agreement between myself and <a href="http://imgur.com/user/CaptainScarlet" target="_blank">@CaptainScarlet</a>, I have to maintain a state of drunkenness while working on this project. Unfortunately alcohol costs money, so I'm graciously accepting any form of assistance in this fine endeavour. If you want to shut up and give me money you can donate via PayPal. If you include your username on the review page I'll give you a shout-out on a Thank You page once things take off!</p>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="A4U5UWH8RXR3E">
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <p>&copy; <?php echo date('Y'); ?> Joe Purdy</p>
+            </footer>
+        </div>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-43101508-5', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
